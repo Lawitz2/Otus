caf5b0da52d37346a43ea6b3687ac6a255ebe7d1
Lawitz2 <eldrasseil@gmail.com>
2024-10-01 15:50:18 +0300

    hw02 w/ bonus task

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,48 +11,75 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(text string) (string, error) {
-	if len(text) < 1 { // empty string check
+	textRunes := []rune(text)
+	if len(textRunes) < 1 { // empty input check
 		return "", nil
 	}
 
-	textRunes := []rune(text)
-
 	numbers := []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-
-	if slices.Contains(numbers, rune(text[0])) { // if the 1st symbol is a number - invalid string
+	if slices.Contains(numbers, textRunes[0]) {
 		return "", ErrInvalidString
 	}
-
 	strBuilder := strings.Builder{}
-	var symbol rune
-	var index int
-	afterDigit := false
+	var escape, prtFlag, numFlag bool
+	var output rune
 
-	for index, symbol = range textRunes[1:] {
-		if slices.Contains(numbers, symbol) {
-			if afterDigit { // if two numbers in a row - invalid string
+	if textRunes[0] == '\\' {
+		escape = true
+	} else {
+		output = textRunes[0]
+		prtFlag = true
+	}
+
+	for _, symbol := range textRunes[1:] {
+		if escape {
+			switch {
+			case symbol != '\\' && !slices.Contains(numbers, symbol):
+				return "", ErrInvalidString
+			default:
+				output = symbol
+				escape = false
+				continue
+			}
+		}
+
+		switch {
+		case symbol == '\\':
+			numFlag = false
+			escape = true
+			if !prtFlag {
+				prtFlag = true
+				continue
+			}
+			strBuilder.WriteRune(output)
+		case slices.Contains(numbers, symbol):
+			if numFlag {
 				return "", ErrInvalidString
 			}
-
 			repeats, err := strconv.Atoi(string(symbol))
 			if err != nil {
 				slog.Error(err.Error())
-				return "", err
 			}
-
-			strBuilder.WriteString(strings.Repeat(string(textRunes[index]), repeats))
-			afterDigit = true
-		} else {
-			if afterDigit {
-				afterDigit = false
+			strBuilder.WriteString(strings.Repeat(string(output), repeats))
+			prtFlag = false
+			numFlag = true
+		default:
+			numFlag = false
+			if !prtFlag {
+				prtFlag = true
+				output = symbol
 				continue
 			}
-			strBuilder.WriteRune(textRunes[index])
+			strBuilder.WriteRune(output)
+			output = symbol
 		}
 	}
 
-	if !slices.Contains(numbers, symbol) { // append last symbol of the input text if it is not a number
-		strBuilder.WriteRune(symbol) // (if it is a number - it'll be handled by the for loop)
+	switch { // handling last symbol (print it out if it wasn't, throw error if escaping end of string)
+	case escape:
+		return "", ErrInvalidString
+	case prtFlag:
+		strBuilder.WriteRune(output)
 	}
 
 	return strBuilder.String(), nil
