69c965a43aea6f502a1599bcc180d910d1611464
Lawitz2 <eldrasseil@gmail.com>
2024-10-03 14:35:16 +0300

    hw03_done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,45 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"slices"
+	"strings"
+)
+
+func Top10(text string) []string {
+	freqMap := make(map[string]int)
+
+	textSlice := strings.Fields(text)
+	slices.Sort(textSlice)
+
+	for _, val := range textSlice {
+		freqMap[val] += 1
+	}
+
+	freqSlice := make([]int, 0, len(freqMap))
+
+	for _, val := range freqMap {
+		freqSlice = append(freqSlice, val)
+	}
+
+	slices.SortFunc(freqSlice, func(a, b int) int {
+		return b - a
+	})
+	freqSlice = freqSlice[:min(10, len(freqSlice))]
+
+	result := make([]string, 0, min(len(freqMap), 10))
+	box := make([]string, 0, cap(result))
+
+	for i := 0; i < cap(result); {
+		for key, val := range freqMap {
+			if val == freqSlice[i] {
+				box = append(box, key)
+				delete(freqMap, key)
+			}
+		}
+		slices.Sort(box)
+		result = append(result, box...)
+		i += len(box)
+		box = box[len(box):]
+	}
+	return result
 }
