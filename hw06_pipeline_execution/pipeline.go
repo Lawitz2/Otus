23f2772b52c0385c8f59eafccd86488f9ef16f93
Lawitz2 <eldrasseil@gmail.com>
2024-10-13 03:37:29 +0300

    hw06 in semi-working state, to be fixed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -6,9 +6,46 @@ type (
 	Bi  = chan interface{}
 )
 
+/*
+TODO: look at TestAllStageStop test, gets stuck on that one
+*/
+
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	channels := make([]Bi, 0, len(stages)+1)
+
+	buffer := make(Bi)
+	go func() {
+		for v := range in {
+			buffer <- v
+		}
+		close(buffer)
+	}()
+	channels = append(channels, buffer)
+
+	for range len(stages) {
+		ch := make(Bi)
+		channels = append(channels, ch)
+	}
+
+	for i, stage := range stages {
+		go func(stage Stage, i int) {
+			ch := stage(channels[i])
+			for {
+				select {
+				case v, ok := <-ch:
+					if !ok {
+						close(channels[i+1])
+						return
+					}
+					channels[i+1] <- v
+				case <-done:
+					close(channels[i+1])
+					return
+				}
+			}
+		}(stage, i)
+	}
+	return channels[len(channels)-1]
 }
