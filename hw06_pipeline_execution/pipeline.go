c8b1e0329e7d88ceed7b946e4b05fabea0e0c1bb
Lawitz2 <eldrasseil@gmail.com>
2024-10-13 23:45:01 +0300

    Update pipeline.go
    Improved algorithm, it's now much simpler while working better.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,33 +10,16 @@ type (
 
 type Stage func(in In) (out Out)
 
+var wg = &sync.WaitGroup{}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	channels := make([]Bi, 0, len(stages)) // слайс каналов, которыми будут соединены stages
-	for range len(stages) {
-		channels = append(channels, make(Bi))
-	}
-
-	// обработка первого stage отдельно, т.к. канал in (type In)
-	// не влезает в слайс каналов (type Bi)
-	go func() {
-		defer close(channels[0])
-		for v := range stages[0](in) {
-			channels[0] <- v
-		}
-	}()
-
-	for i, stage := range stages[1:] {
-		go func() {
-			defer close(channels[i+1])
-			for v := range stage(channels[i]) {
-				channels[i+1] <- v
-			}
-		}()
-	}
-
 	output := make(Bi)
+	for _, stage := range stages {
+		out := stage(in)
+		in = out
+	}
 
-	go filter(channels[len(channels)-1], done, output)
+	go filter(in, done, output)
 
 	return output
 }
@@ -47,6 +32,7 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 Как только появляется сигнал done - output закрывается, а остальные
 значения выбрасываются для graceful shutdown горутин.
 */
+
 func filter(in In, done In, output Bi) {
 	defer close(output)
 	for {
