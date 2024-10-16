abf2e2a579626ccaa3aa87e326d7b8e2f7955919
Lawitz2 <eldrasseil@gmail.com>
2024-10-17 01:00:11 +0300

    hw07 completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,15 +1,109 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeOffset        = errors.New("negative offset value")
+	ErrNegativeLimit         = errors.New("negative limit value")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	fileDescr, err := os.Stat(from) // getting filesize.
+	if err != nil {
+		slog.Error(err.Error())
+		return ErrUnsupportedFile
+	}
+
+	// check for input validity
+	if fileDescr.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+
+	targetFile, err := os.Open(fromPath)
+	if err != nil {
+		slog.Error(err.Error())
+		return ErrUnsupportedFile
+	}
+	defer targetFile.Close()
+
+	copyFile, err := os.Create(toPath)
+	if err != nil {
+		slog.Error(err.Error())
+		return ErrUnsupportedFile
+	}
+	defer copyFile.Close()
+
+	reader := bufio.NewReader(targetFile)
+	writer := bufio.NewWriter(copyFile)
+
+	// skip first n bytes based on offset value. 0 will have no effect.
+	_, err = reader.Discard(int(offset))
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	// the following math allows me to have nil error after successful copy.
+	var size int64
+	if limit == 0 {
+		size = fileDescr.Size() - offset
+	} else {
+		size = min(limit, fileDescr.Size()-offset)
+	}
+
+	prBar := newProgressBar(size)
+
+	// Copying contents of original file into a multiwriter.
+	// One part of the multiwriter is writing data into the new file.
+	// The other one feeds data into progress bar for progress tracking.
+	_, err = io.CopyN(io.MultiWriter(writer, prBar), reader, size)
+	writer.Flush()
+
+	fmt.Println("\ndone!")
+
+	return err
+}
+
+// progress bar struct
+// Progress represents how much was already copied
+// Capacity represents total amount that needs to be copied.
+type pBar struct {
+	Progress int64
+	Capacity int64
+}
+
+func newProgressBar(capacity int64) *pBar {
+	return &pBar{
+		Progress: 0,
+		Capacity: capacity,
+	}
+}
+
+// implementation of io.Writer interface
+// shows "bytes copied/bytes total", a progress bar with 20 segments (each representing 5%)
+// and the percentage of copying progress.
+func (p *pBar) Write(b []byte) (n int, err error) {
+	p.Progress += int64(len(b))
+	done := int(20 * p.Progress / p.Capacity)
+	notDone := 20 - done
+	percentage := 100 * p.Progress / p.Capacity
+	//nolint:lll
+	fmt.Printf("\rcopying %d/%d [%s%s], %d%%", p.Progress, p.Capacity, strings.Repeat("█", done), strings.Repeat("-", notDone), percentage)
+	return len(b), nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,150 @@
 package main
 
-import "testing"
+import (
+	"log/slog"
+	"os"
+	"testing"
 
+	"github.com/stretchr/testify/require"
+)
+
+//nolint:goconst
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("just a file copy", func(t *testing.T) {
+		from = ".\\testdata\\input.txt"
+		to = ".\\testdata\\output.txt"
+		inInfo, err := os.Stat(from)
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+
+		err = Copy(from, to, 0, 0)
+		require.Nil(t, err)
+
+		outInfo, err := os.Stat(to)
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+
+		require.Equal(t, outInfo.Size(), inInfo.Size())
+		os.Remove(to)
+	})
+
+	t.Run("offset and limit", func(t *testing.T) {
+		from = ".\\testdata\\input.txt"
+		to = ".\\testdata\\output2.txt"
+		offset = 150
+		limit = 500
+		inInfo, err := os.Stat(from)
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+
+		err = Copy(from, to, offset, 0)
+		require.Nil(t, err)
+
+		outInfo, err := os.Stat(to)
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+
+		require.Equal(t, outInfo.Size(), inInfo.Size()-offset)
+		os.Remove(to)
+
+		err = Copy(from, to, offset, limit)
+		require.Nil(t, err)
+
+		outInfo, _ = os.Stat(to)
+		require.Equal(t, outInfo.Size(), min(limit, inInfo.Size()-offset))
+		os.Remove(to)
+
+		limit = inInfo.Size() * 2 // limit higher than file size
+		err = Copy(from, to, offset, limit)
+		require.Nil(t, err)
+
+		outInfo, _ = os.Stat(to)
+		require.Equal(t, outInfo.Size(), min(limit, inInfo.Size()-offset))
+		os.Remove(to)
+	})
+
+	t.Run("comparison to pre-made test files", func(t *testing.T) {
+		from = ".\\testdata\\input.txt"
+		to = ".\\testdata\\output.txt"
+		testA := ".\\testdata\\out_offset0_limit0.txt"
+		testB := ".\\testdata\\out_offset0_limit10.txt"
+		testC := ".\\testdata\\out_offset0_limit1000.txt"
+		testD := ".\\testdata\\out_offset0_limit10000.txt"
+		testE := ".\\testdata\\out_offset100_limit1000.txt"
+		testF := ".\\testdata\\out_offset6000_limit1000.txt"
+
+		err := Copy(from, to, 0, 0) // to out_offset0_limit0.txt
+		require.Nil(t, err)
+		outInfo, _ := os.Stat(from)
+		exampleInfo, _ := os.Stat(testA)
+		os.SameFile(outInfo, exampleInfo)
+		os.Remove(to)
+
+		err = Copy(from, to, 0, 10) // to out_offset0_limit0.txt
+		require.Nil(t, err)
+		outInfo, _ = os.Stat(from)
+		exampleInfo, _ = os.Stat(testB)
+		os.SameFile(outInfo, exampleInfo)
+		os.Remove(to)
+
+		err = Copy(from, to, 0, 1000) // to out_offset0_limit1000.txt
+		require.Nil(t, err)
+		outInfo, _ = os.Stat(from)
+		exampleInfo, _ = os.Stat(testC)
+		os.SameFile(outInfo, exampleInfo)
+		os.Remove(to)
+
+		err = Copy(from, to, 0, 10000) // to out_offset0_limit10000.txt
+		require.Nil(t, err)
+		outInfo, _ = os.Stat(from)
+		exampleInfo, _ = os.Stat(testD)
+		os.SameFile(outInfo, exampleInfo)
+		os.Remove(to)
+
+		err = Copy(from, to, 0, 10) // to out_offset100_limit1000.txt
+		require.Nil(t, err)
+		outInfo, _ = os.Stat(from)
+		exampleInfo, _ = os.Stat(testE)
+		os.SameFile(outInfo, exampleInfo)
+		os.Remove(to)
+
+		err = Copy(from, to, 0, 10) // to out_offset6000_limit1000.txt
+		require.Nil(t, err)
+		outInfo, _ = os.Stat(from)
+		exampleInfo, _ = os.Stat(testF)
+		os.SameFile(outInfo, exampleInfo)
+		os.Remove(to)
+	})
+
+	t.Run("incorrect requests", func(t *testing.T) {
+		from = ".\\testdata\\input.txt"
+		to = ".\\testdata\\output2.txt"
+		offset = 150
+		limit = 500
+		inInfo, _ := os.Stat(from)
+
+		err := Copy(from+"randomstuff", to, offset, limit) // trying to copy a non-existing file
+		require.Equal(t, err, ErrUnsupportedFile)
+
+		err = Copy(from, to, inInfo.Size()*2, limit) // offset higher than the whole file
+		require.Equal(t, err, ErrOffsetExceedsFileSize)
+
+		err = Copy(from, to, -5, limit) // negative offset value
+		require.Equal(t, err, ErrNegativeOffset)
+
+		err = Copy(from, to, offset, -400) // negative limit value
+		require.Equal(t, err, ErrNegativeLimit)
+
+		// confirm that none of the incorrect requests created a new file
+		_, err = os.Stat(to)
+		require.True(t, os.IsNotExist(err))
+	})
 }
