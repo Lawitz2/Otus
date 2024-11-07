bdb53fd70da5e90c84d2ca748f555a0d39b2d9b4
Lawitz2 <eldrasseil@gmail.com>
2024-11-08 02:31:46 +0300

    Idk

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"log/slog"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -10,7 +18,40 @@ type EnvValue struct {
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
-func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+func ReadDir(dirPath string) (Environment, error) {
+	dir, err := os.ReadDir(dirPath)
+	if err != nil {
+		slog.Error(err.Error())
+		return nil, err
+	}
+	os.Chdir(dirPath)
+
+	env := make(Environment, len(dir))
+	for _, entry := range dir {
+		ev := EnvValue{}
+		f, _ := os.Open(entry.Name())
+		if strings.Contains(f.Name(), "=") {
+			continue
+		}
+
+		fStat, _ := f.Stat()
+		if fStat.Size() == 0 {
+			ev.NeedRemove = true
+		}
+
+		scanner := bufio.NewScanner(f)
+		scanner.Scan()
+
+		ev.Value = string(bytes.Replace([]byte(scanner.Text()), []byte{0}, []byte{10}, -1))
+
+		env[f.Name()] = ev
+	}
+
+	for key, val := range env {
+		if val.NeedRemove {
+			delete(env, key)
+		}
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
+	returnCode = 1
+	c := exec.Command(cmd[0], cmd[1:]...)
+	c.Env = os.Environ()
+	envSlice := make([]string, 0, len(env))
+	for key, e := range env {
+		envSlice = append(envSlice, key+"="+e.Value)
+	}
+	c.Env = append(c.Env, envSlice...)
+	err := c.Start()
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	c.Wait()
+	returnCode = 0
 	return
 }
