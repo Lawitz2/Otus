b43a8485e8ddf36c9d64e63ce180ec1f4588a1c5
Lawitz2 <eldrasseil@gmail.com>
2024-11-29 16:08:47 +0300

    hw10 done, still need more tests

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,10 +1,9 @@
 package hw10programoptimization
 
 import (
+	"bufio"
 	"encoding/json"
-	"fmt"
 	"io"
-	"regexp"
 	"strings"
 )
 
@@ -20,47 +19,110 @@ type User struct {
 
 type DomainStat map[string]int
 
+const (
+	singleBytes = iota
+	lines
+	jsons
+)
+
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
+	const algorithm = jsons
+
+	switch algorithm {
+	case singleBytes:
+		return domainStatBytes(r, domain)
+	case lines:
+		return domainStatLines(r, domain)
+	case jsons:
+		return domainStatJSON(r, domain)
 	}
-	return countDomains(u, domain)
+	return nil, nil
 }
 
-type users [100_000]User
+// read individual bytes, designed only to find email addresses
+// average speed, low memory consumption.
+func domainStatBytes(r io.Reader, domain string) (DomainStat, error) {
+	var err error
+	var s byte
+	var at, dot bool
+	builder, altBuilder := strings.Builder{}, strings.Builder{}
+	dStat := make(DomainStat)
+	rBuf := bufio.NewReader(r)
 
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
+	for err == nil {
+		s, err = rBuf.ReadByte()
+		// case order is important
+		switch {
+		case s == '@':
+			at = true
+		case at && s == '.':
+			dot = true
+			builder.WriteByte(s)
+		case at && dot && s == '"':
+			at, dot = false, false
+			if altBuilder.String() == domain {
+				builder.WriteString(altBuilder.String())
+				dStat[strings.ToLower(builder.String())]++
+			}
+			builder.Reset()
+			altBuilder.Reset()
+		case at && dot:
+			altBuilder.WriteByte(s)
+		case at:
+			builder.WriteByte(s)
 		}
-		result[i] = user
 	}
-	return
+
+	if err == io.EOF {
+		return dStat, nil
+	}
+	return nil, err
 }
 
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
+// read line by line, designed to find only email addresses
+// higher speed, higher memory consumption.
+func domainStatLines(r io.Reader, domain string) (DomainStat, error) {
+	var err error
+	var b string
+	var start, end, dotPos int
+	dStat := make(DomainStat)
+	rBuf := bufio.NewReader(r)
 
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
+	for err == nil {
+		b, err = rBuf.ReadString('\n')
+		start = strings.Index(b, "@") + 1
+		end = strings.Index(b[start:], `"`) + start
+		dotPos = strings.Index(b[start:end], `.`)
+		if b[start+dotPos+1:end] == domain {
+			dStat[strings.ToLower(b[start:end])]++
+		}
+	}
+
+	if err == io.EOF {
+		return dStat, nil
+	}
+	return nil, err
+}
+
+// parses JSONs, as a result data other than email can also be used
+// the slowest option, average memory consumption
+// can be improved using 3rd party json parsers, but since it fits into
+// requirements i decided to stick with the standard library option.
+func domainStatJSON(r io.Reader, domain string) (DomainStat, error) {
+	dStat := make(DomainStat)
+	decoder := json.NewDecoder(r)
+	user := &User{}
+	var err error
+	for {
+		err = decoder.Decode(user)
+		if err == io.EOF {
+			return dStat, nil
+		}
 		if err != nil {
 			return nil, err
 		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
+		if strings.Contains(user.Email, "."+domain) {
+			dStat[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]++
 		}
 	}
-	return result, nil
 }
--- hw10_program_optimization/stats_test.go
+++ hw10_program_optimization/stats_test.go
@@ -1,3 +1,4 @@
+//go:build !bench
 // +build !bench
 
 package hw10programoptimization
@@ -36,4 +37,6 @@ func TestGetDomainStat(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, DomainStat{}, result)
 	})
+
+	// TODO: MORE TESTS
 }
