5b7506a98026fb945e81f90acf3a1595299aaa9a
Lawitz2 <eldrasseil@gmail.com>
2024-12-14 01:00:40 +0300

    env fix

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -18,25 +18,72 @@ type EnvValue struct {
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
-func ReadDir(dirPath string) (Environment, error) {
-	dir, err := os.ReadDir(dirPath)
+//func ReadDir(dirPath string) (Environment, error) {
+//	dir, err := os.ReadDir(dirPath)
+//	if err != nil {
+//		slog.Error(err.Error())
+//		return nil, err
+//	}
+//	os.Chdir(dirPath)
+//
+//	env := make(Environment, len(dir))
+//	for _, entry := range dir {
+//		ev := EnvValue{}
+//		f, _ := os.Open(entry.Name())
+//		if strings.Contains(f.Name(), "=") {
+//			continue
+//		}
+//
+//		fStat, _ := f.Stat()
+//		if fStat.Size() == 0 {
+//			ev.NeedRemove = true
+//		}
+//
+//		scanner := bufio.NewScanner(f)
+//		scanner.Scan()
+//
+//		ev.Value = string(bytes.ReplaceAll([]byte(scanner.Text()), []byte{0}, []byte{10}))
+//		ev.Value = strings.TrimRight(ev.Value, "\t ")
+//
+//		env[f.Name()] = ev
+//		f.Close()
+//	}
+//
+//	//for key, val := range env {
+//	//	if val.NeedRemove {
+//	//		delete(env, key)
+//	//	}
+//	//}
+//
+//	return env, nil
+//}
+
+func ReadDir(dir string) (Environment, error) {
+	fsDir, err := os.ReadDir(dir)
 	if err != nil {
-		slog.Error(err.Error())
 		return nil, err
 	}
-	os.Chdir(dirPath)
 
 	env := make(Environment, len(dir))
-	for _, entry := range dir {
-		ev := EnvValue{}
-		f, _ := os.Open(entry.Name())
-		if strings.Contains(f.Name(), "=") {
+
+	for _, entry := range fsDir {
+		if strings.Contains(entry.Name(), "=") {
+			continue // "=" не должно быть в имени файла
+		}
+
+		f, err := os.Open(dir + "/" + entry.Name())
+		if err != nil {
+			slog.Error("Не могу открыть файл", "error", err)
 			continue
 		}
+
+		ev := EnvValue{}
 
 		fStat, _ := f.Stat()
 		if fStat.Size() == 0 {
 			ev.NeedRemove = true
+			env[entry.Name()] = ev
+			continue
 		}
 
 		scanner := bufio.NewScanner(f)
@@ -45,15 +92,8 @@ func ReadDir(dirPath string) (Environment, error) {
 		ev.Value = string(bytes.ReplaceAll([]byte(scanner.Text()), []byte{0}, []byte{10}))
 		ev.Value = strings.TrimRight(ev.Value, "\t ")
 
-		env[f.Name()] = ev
-		f.Close()
+		env[entry.Name()] = ev
 	}
-
-	//for key, val := range env {
-	//	if val.NeedRemove {
-	//		delete(env, key)
-	//	}
-	//}
 
 	return env, nil
 }
