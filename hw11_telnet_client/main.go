bdbe3a4efa0762b2b74c1988c771e9554aca21e6
Lawitz2 <eldrasseil@gmail.com>
2025-01-17 20:54:05 +0300

    telnet draft

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,71 @@
 package main
 
+import (
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var wg sync.WaitGroup
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	wg.Add(1)
+	go tcpserver()
+
+	tc := NewTelnetClient("localhost:81", time.Second*15, os.Stdin, os.Stdout)
+	wg.Add(1)
+	tc.Connect()
+	go tc.Send()
+	go tc.Receive()
+
+	done := make(chan os.Signal, 1)
+	go func() {
+		signal.Notify(done, syscall.SIGINT)
+		<-done
+		slog.Info("received interrupt signal")
+		tc.Close()
+		os.Exit(0)
+	}()
+
+	wg.Wait()
+	//time.Sleep(time.Second * 2)
+}
+
+func tcpserver() {
+	defer wg.Done()
+
+	listener, err := net.Listen("tcp", `localhost:81`)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			slog.Error(err.Error())
+			return
+		}
+		go func() {
+			slog.Info("received connection", "source", conn.RemoteAddr())
+			buf := make([]byte, 4096)
+			for {
+				n, err := conn.Read(buf)
+				slog.Info("server received " + strconv.Itoa(n) + " bytes")
+				slog.Debug(string(buf))
+				if err != nil {
+					slog.Error("server error", "err", err.Error())
+					return
+				}
+				time.Sleep(time.Second / 10)
+				conn.Close()
+			}
+		}()
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"log/slog"
+	"net"
 	"time"
 )
 
@@ -12,9 +15,69 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type TelnetCl struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (t *TelnetCl) Connect() error {
+	tcpConn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		slog.Error("couldn't connect", "err", err.Error())
+		return err
+	}
+	t.conn = tcpConn
 	return nil
+}
+
+func (t *TelnetCl) Close() error {
+	defer wg.Done()
+	err := t.conn.Close()
+	if err != nil {
+		slog.Error("error closing connection", "err", err.Error())
+		return err
+	}
+	return nil
+}
+
+func (t *TelnetCl) Send() error {
+	slog.Info("sending data")
+	scanner := bufio.NewScanner(t.in)
+	var err error
+	for scanner.Scan() {
+		_, err = t.conn.Write([]byte(scanner.Text()))
+		if err != nil {
+			slog.Error(err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+func (t *TelnetCl) Receive() error {
+	slog.Info("reading connection")
+	scanner := bufio.NewScanner(t.conn)
+	var err error
+	for scanner.Scan() {
+		_, err = t.out.Write([]byte(scanner.Text()))
+		if err != nil {
+			slog.Error(err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &TelnetCl{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
 }
 
 // Place your code here.
